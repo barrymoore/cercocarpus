d3fb8b5ef5903a2fb6fa5c26cbbc472d746f8ce6
barrymoore <barry.moore@genetics.utah.edu>
2021-04-25 19:32:57 -0600

    Clean up and initial edits to add echo deleted:    ../cercocarpus.py modified:   cercocarpus.py deleted:    cli.py deleted:    ../cli.py

--- cercocarpus/cercocarpus.py
+++ cercocarpus/cercocarpus.py
@@ -1,1 +1,20 @@
-"""Main module."""
+#! /usr/bin/env python
+import fire
+import sys
+
+class Cercocarpus():
+
+  def echo_me(self, file):
+    
+    fh = None
+    if (file == '-'):
+      fh = sys.stdin
+    else:
+      fh = open(file, 'r')
+      
+    for line in fh: 
+      print(line)
+
+if __name__ == '__main__':
+  cerc = Cercocarpus()
+  fire.Fire({'echo': cerc.echo_me})
