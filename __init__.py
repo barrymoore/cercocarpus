522cd1cb2211e5ddc53c994c363239b1dc9925f6
barrymoore <barry.moore@genetics.utah.edu>
2021-04-26 00:57:13 -0600

    bumping version 0.2.0 deleted:    __init__.py modified:   cercocarpus/__init__.py modified:   setup.cfg modified:   setup.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for cercocarpus."""
-
-__author__ = """Barry Moore"""
-__email__ = 'barry.moore@genetics.utah.edu'
-__version__ = '0.1.0'
--- cercocarpus/__init__.py
+++ cercocarpus/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Barry Moore"""
 __email__ = 'barry.moore@genetics.utah.edu'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
