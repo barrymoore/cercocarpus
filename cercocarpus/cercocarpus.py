be0f7fcf14a4e346440483739f9ff764bd72c619
barrymoore <barry.moore@genetics.utah.edu>
2021-04-26 00:32:33 -0600

    Cleaning up whitespace to maker lint happy modified:   cercocarpus/cercocarpus.py

--- cercocarpus/cercocarpus.py
+++ cercocarpus/cercocarpus.py
@@ -2,67 +2,68 @@
 import fire
 import sys
 
+
 class Cercocarpus(object):
 
-  def __init__(self):
-    pass
-    
-  def echo(self, file='-'):
-    
-    fh = None
-    if (file == '-'):
-      fh = sys.stdin
-    else:
-      fh = open(file, 'r')
-      
-    for line in fh:
-      print(line.rstrip())
+    def __init__(self):
+        pass
 
-  def txt2comma(self, file='-'):
-    
-    fh = None
-    if (file == '-'):
-      fh = sys.stdin
-    else:
-      fh = open(file, 'r')
+    def echo(self, file='-'):
 
-    rows = [x.rstrip() for x in fh.readlines()]
-    print(','.join(rows))
+        fh = None
+        if (file == '-'):
+            fh = sys.stdin
+        else:
+            fh = open(file, 'r')
 
-  def txt2table(self, file='-'):
-    
-    import pandas as pd
-    from tabulate import tabulate
-    
-    fh = None
-    if (file == '-'):
-      fh = sys.stdin
-    else:
-      fh = open(file, 'r')
+        for line in fh:
+            print(line.rstrip())
 
-    df = pd.read_table(fh, header=None)
-    print(tabulate(df.values.tolist()))
+    def txt2comma(self, file='-'):
 
-  def transpose(self, file='-'):
-    
-    import pandas as pd
-    
-    fh = None
-    if (file == '-'):
-      fh = sys.stdin
-    else:
-      fh = open(file, 'r')
+        fh = None
+        if (file == '-'):
+            fh = sys.stdin
+        else:
+            fh = open(file, 'r')
 
-    df = pd.read_table(fh, header=None).transpose()
-    for index, row in df.iterrows():
-      print('\t'.join([str(x) for x in row.tolist()]))
+        rows = [x.rstrip() for x in fh.readlines()]
+        print(','.join(rows))
 
-      
+    def txt2table(self, file='-'):
+
+        import pandas as pd
+        from tabulate import tabulate
+
+        fh = None
+        if (file == '-'):
+            fh = sys.stdin
+        else:
+            fh = open(file, 'r')
+
+        df = pd.read_table(fh, header=None)
+        print(tabulate(df.values.tolist()))
+
+    def transpose(self, file='-'):
+
+        import pandas as pd
+
+        fh = None
+        if (file == '-'):
+            fh = sys.stdin
+        else:
+            fh = open(file, 'r')
+
+        df = pd.read_table(fh, header=None).transpose()
+        for index, row in df.iterrows():
+            print('\t'.join([str(x) for x in row.tolist()]))
+
+
 if __name__ == '__main__':
-  cerc = Cercocarpus()
-  fire.Fire({
-    'echo': cerc.echo,
-    'txt2comma': cerc.txt2comma,
-    'txt2table': cerc.txt2table,
-    'transpose': cerc.transpose,
-  })
+    cerc = Cercocarpus()
+    fire.Fire({
+        'echo': cerc.echo,
+        'txt2comma': cerc.txt2comma,
+        'txt2table': cerc.txt2table,
+        'transpose': cerc.transpose,
+     })
