366a8e182051bd3f8c32c0e3432e73452554998c
barrymoore <barry.moore@genetics.utah.edu>
2021-04-25 23:22:17 -0600

    Added txt2table and transpose to cercocarpus/cercocarpus.py

--- cercocarpus/cercocarpus.py
+++ cercocarpus/cercocarpus.py
@@ -29,8 +29,40 @@ class Cercocarpus(object):
     rows = [x.rstrip() for x in fh.readlines()]
     print(','.join(rows))
 
+  def txt2table(self, file='-'):
+    
+    import pandas as pd
+    from tabulate import tabulate
+    
+    fh = None
+    if (file == '-'):
+      fh = sys.stdin
+    else:
+      fh = open(file, 'r')
+
+    df = pd.read_table(fh, header=None)
+    print(tabulate(df.values.tolist()))
+
+  def transpose(self, file='-'):
+    
+    import pandas as pd
+    
+    fh = None
+    if (file == '-'):
+      fh = sys.stdin
+    else:
+      fh = open(file, 'r')
+
+    df = pd.read_table(fh, header=None).transpose()
+    for index, row in df.iterrows():
+      print('\t'.join([str(x) for x in row.tolist()]))
+
       
 if __name__ == '__main__':
   cerc = Cercocarpus()
-  fire.Fire({'echo': cerc.echo,
-             'txt2comma': cerc.txt2comma})
+  fire.Fire({
+    'echo': cerc.echo,
+    'txt2comma': cerc.txt2comma,
+    'txt2table': cerc.txt2table,
+    'transpose': cerc.transpose,
+  })
