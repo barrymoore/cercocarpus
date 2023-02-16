483355018075ddb8c5d976e7b4e8f09d9ebf674f
Barry Moore <barry.moore@genetics.utah.edu>
2023-02-16 13:19:28 -0700

    Updated cercocarpus/proband_gene_phenotype_overlap.py to a version relatively stable alpha version - still more work to do

--- cercocarpus/proband_gene_phenotype_overlap.py
+++ cercocarpus/proband_gene_phenotype_overlap.py
@@ -73,28 +73,7 @@ def main():
                            names=['id', 'term', 'mim', 'gene', 'source', 'source_id'])
     df_p2g.drop_duplicates(subset=['id', 'gene'], inplace=True)
     df_p2g = df_p2g.set_index('id')
-    
-    # # Parse HPO Genes_to_Phenotype File to Dataframe
-    # df_g2p = pd.read_table(g2p,
-    #                        skiprows=1,
-    #                        header=None,
-    #                        index_col=False,
-    #                        names=['gene_id', 'gene', 'id', 'term', 'freq_raw', 'freq_hpo', 'info_gd_source',
-    #                               'gd_source', 'disease_id'])
-    # df_g2p.drop_duplicates(subset=['id', 'gene'], inplace=True)
-    # df_g2p = df_g2p.set_index('id')
-    
-    # # Parse HPO Phenotpye File to Dataframe
-    # 
-    # df_phen = pd.read_table(phen,
-    #                        skiprows=5,
-    #                        header=None,
-    #                        index_col=False,
-    #                        names=['disease_id', 'disease', 'qualifier', 'id', 'reference', 'evidence', 'onset',
-    #                               'frequency', 'sex', 'modifier', 'aspect', 'biocuration'])
-    # df_phen.drop_duplicates(subset=['id', 'disease_id'], inplace=True)
-    # df_phen = df_phen.set_index('id')
-    
+        
     # Get term frequency and information content (ic)
     term_counts = pd.DataFrame(df_p2g.reset_index()['id'].value_counts())
     term_counts.rename(columns={'id': 'count'}, inplace=True)
@@ -103,7 +82,6 @@ def main():
     term_counts['ic'] = -1 * np.log10(term_counts['freq'])
     term_counts.loc['HP:0000118'] = [term_counts['count'].sum(), 1, 0]
     df_p2g = df_p2g.join(term_counts)
-    #df_g2p = df_g2p.join(term_counts)
     
     # Parse HPO JSON
     with open(args.json_file) as json_fh:
@@ -163,44 +141,50 @@ def main():
 
     #--------------------------------------------------------------------------------
 
-    # Get subgraph/leaves of gene HPO ancestors
-    gene = df_cnd.iloc[0]['gene']
-    gene_ids = set(df_p2g.query('gene == @gene').index.to_list())
-    gene_id_ancestors = set()
-    for id in gene_ids:
-        gene_id_ancestors.add(id)
-        if id not in pabG.nodes:
-            # WARN
-            continue
-        gene_id_ancestors.update(nx.ancestors(pabG, id))
-    gene_id_ancestors.add('HP:0000118')
-    genG = pabG.subgraph(gene_id_ancestors)
-    gen_leaves = [node for node in genG.nodes() if genG.out_degree(node)==0]
-
-    # Get subgraph of proband & gene IDs
-    prb_gene_ids = prb_id_ancestors.union(gene_id_ancestors)
-    pgG = pabG.subgraph(prb_gene_ids)
-    pgG_ids = set(pgG.nodes)
-    pgUG = nx.Graph(pgG)
+    all_df_lcas = []
+    for gene in df_cnd['gene']:
     
-    gene_chunks = np.array_split(gen_leaves, 8)
-    all_lcas = []
-    for prb_id in tqdm(prb_leaves):
-        if prb_id not in pgG_ids:
-            # WARN
-            continue
+        # Get subgraph/leaves of gene HPO ancestors
+        # gene = df_cnd.iloc[0]['gene']
+        gene_ids = set(df_p2g.query('gene == @gene').index.to_list())
+        gene_id_ancestors = set()
+        for id in gene_ids:
+            gene_id_ancestors.add(id)
+            if id not in pabG.nodes:
+                # WARN
+                continue
+            gene_id_ancestors.update(nx.ancestors(pabG, id))
+        gene_id_ancestors.add('HP:0000118')
+        genG = pabG.subgraph(gene_id_ancestors)
+        gen_leaves = [node for node in genG.nodes() if genG.out_degree(node)==0]
+    
+        # Get subgraph of proband & gene IDs
+        prb_gene_ids = prb_id_ancestors.union(gene_id_ancestors)
+        pgG = pabG.subgraph(prb_gene_ids)
+        pgG_ids = set(pgG.nodes)
+        pgUG = nx.Graph(pgG)
         
-        # Check for gene_ids in genG_ids
-        gene_lcas = []
-        lcas = Parallel(n_jobs=args.jobs)(delayed(get_lcas)(prb_id, genes, pgG, pgUG) for genes in gene_chunks)
-        for lca in lcas:
-            all_lcas.extend(lca)
+        all_gene_lcas = []
+        gene_chunks = np.array_split(gen_leaves, args.jobs * 2)
+        for prb_id in tqdm(prb_leaves, desc=gene):
+            if prb_id not in pgG_ids:
+                # WARN
+                continue
+            
+            # Check for gene_ids in genG_ids
+            lcas = Parallel(n_jobs=args.jobs)(delayed(get_lcas)(prb_id, genes, pgG, pgUG) for genes in gene_chunks)
+            for lca in lcas:
+                all_gene_lcas.extend(lca)
+    
+        df_lcas = pd.DataFrame(all_gene_lcas, columns=['anc_id', 'prb_id', 'gene_id', 'lca_id', 'shpl'])
+        df_lcas['gene'] = gene
+        all_df_lcas.append(df_lcas)
 
-    df_lcas = pd.DataFrame(all_lcas, columns=['prb_id', 'gene_id', 'lca_id', 'shpl'])
+    df_lcas = pd.concat(all_df_lcas)
     df_lcas.set_index('lca_id', inplace=True)
     df_lcas = df_lcas.join(term_counts, how='left')
     df_lcas.sort_values(by='ic', ascending=False, inplace=True)
-    df_lcas.drop_duplicates(subset='prb_id', keep='first', inplace=True)
+    df_lcas.drop_duplicates(subset=['prb_id', 'gene'], keep='first', inplace=True)
 
     df_lcas = df_lcas.merge(df_p2g['term'], left_on='prb_id', right_index=True).drop_duplicates()
     df_lcas.rename(columns={'term': 'prb_term'}, inplace=True)
@@ -208,208 +192,36 @@ def main():
     df_lcas = df_lcas.merge(df_p2g['term'], left_on='gene_id', right_index=True).drop_duplicates()
     df_lcas.rename(columns={'term': 'gene_term'}, inplace=True)
 
+    df_lcas = df_lcas.merge(df_p2g['term'], left_on='anc_id', right_index=True).drop_duplicates()
+    df_lcas.rename(columns={'term': 'anc_term'}, inplace=True)
+
     df_lcas = df_lcas.merge(df_p2g['term'], left_index=True, right_index=True).drop_duplicates()
     df_lcas.rename(columns={'term': 'lca_term'}, inplace=True)
 
     df_lcas.sort_values(by='ic', ascending=False, inplace=True)
     df_lcas.reset_index(inplace=True, drop=False)
     df_lcas.rename(columns={'index': 'lca_id'}, inplace=True)
-    df_lcas = df_lcas.loc[:,['ic', 'prb_term', 'gene_term', 'lca_term', 'prb_id', 'gene_id', 'lca_id', 'shpl', 'count', 'freq']]
-    
-    print(df_lcas.to_csv(sep='\t'))
-            
-        # prb_gen_lcas = pd.DataFrame(columns=['prb_id', 'gen_id', 'lca_id'])
-        # prb_gene_lcas.append(prb_gene_lca)
-        #if prb_id == gene_id:
-        #    ic = 0
-        #    if prb_id in term_counts.index:
-        #        ic = term_counts.loc[prb_id]['ic']
-        #        prb_gen_lcas.append((prb_id, ic, (prb_id, gene_id)))
-        #    return
-        #
-        #if gene_id not in pgG_ids:
-        #    # WARN
-        #    return
-        #
-        #if lca == 'HP:0000118':
-        #    ic = 0
-        #elif lca in term_counts.index:
-        #    ic = term_counts.loc[lca]['ic']
-        #    prb_gene_lcas = sorted(prb_gen_lcas, key=lambda x: x[1], reverse=True)
-        #    gene_lcas.append(prb_gene_lcas[0])
+    #all_lcas.extend(all_gene_lcas)
 
-    # sys.stderr.write('DEBUG : gene_lcas : ' + str(len(gene_lcas)))
-    # gene_lcas = sorted(gene_lcas, key=lambda x: x[1], reverse=True)
-    # for lca in gene_lcas:
-    #     (lca_id, ic, pair) = lca
-    #     prb_id, gen_id = pair
-    #     lca_term = G.nodes[lca_id]['term']
-    #     prb_term = G.nodes[prb_id]['term']
-    #     gen_term = G.nodes[gen_id]['term']
-    #     print('\t'.join([gene, prb_term, gen_term, lca_term, str(ic)]))
-
-    # # ### Write out Graphvix Dot File of Proband Ancestors Graph
-    # 
-    # #write_dot(prbG,'proband.dot')
-    # # dot -Tpng test.dot > test.png
-    # 
-    # # Trim proband terms Dataframe to leaf node terms
-    # df_prb = df_prb[df_prb.index.isin(prb_trim)]
-    # 
-    # # Expand proband terms Dataframe to include ancestors 
-    # df_ancestors = pd.DataFrame(prbG.nodes, columns=['id'])
-    # df_ancestors.set_index('id', inplace=True)
-    # df_ancestors['review'] = 1
-    # df_ancestors['context'] = 'Ancestor'
-    # df_ancestors = df_ancestors.join(df_p2g['term'].drop_duplicates(), how='left')
-    # 
-    # # Score All HPO Disease Genes Against Proband Terms with Hypergeometric Test
-    # # Expect this step to take ~6 minutes
-    # data = []
-    # all_genes = set(df_p2g[df_p2g.index.isin(G.nodes())]['gene'].to_list())
-    # for gene in tqdm(all_genes):    
-    #     df_gene = df_p2g.query('gene == @gene')
-    #     # df_inter = df_gene.join(df_prb, how='inner', lsuffix='_gene', rsuffix='_proband').reset_index()
-    #     df_inter = df_gene.join(df_ancestors, how='inner', lsuffix='_gene', rsuffix='_proband').reset_index()
-    #     df_inter.drop_duplicates(subset='id', inplace=True, ignore_index=True)
-    # 
-    #     x = len(df_inter.index.to_list())    # Intersection
-    #     M = len(set(df_p2g.index.to_list())) # Total number of terms
-    #     # n = len(df_prb.index.to_list())      # Proband terms
-    #     n = len(df_ancestors.index.to_list())      # Proband ancestor terms
-    #     N = len(df_gene.index.to_list())     # Gene terms
-    # 
-    # 
-    #     pval = hypergeom.sf(x - 1, M, n, N)
-    #     phred = -1 * math.log10(pval)
-    #     
-    #     data.append({'gene': gene, 'x': x, 'n': n, 'N': N, 'pval': pval, 'phred': phred})
-    # 
-    # df_data = pd.DataFrame(data)
-    # 
-    # # Summary statistics for hypergeometric test input/results
-    # print(df_data.describe())
-    # 
-    # # Plot distribution of genes/proband 'term overlap' and -log10(pval) from hypergeometric test
-    # sns.kdeplot(df_data['x']);
-    # sns.kdeplot(df_data['phred']);
-    # 
-    # # Add Percentile Rank Based on -log10(p) of Hypergeometric Test
-    # df_data['pct_rank'] = df_data['phred'].rank(pct = True)
-    # 
-    # # Basic Metrics on Dataset 
-    # print("Candidate Genes:        " + str(len(df_cnd)))
-    # print("Proband Terms:          " + str(len(df_prb)))
-    # print("Proband Ancestor Terms: " + str(len(df_ancestors)))
-    # print("Proband Ancestor Graph: " + str(len(prbG)))
-    # 
-    # # Table of hypergeometric details for candidate gene
-    # df_cnd_mrg = df_data.merge(df_cnd, on='gene')
-    # df_cnd_mrg.rename(columns={'x': 'overlap', 'n': 'prb_terms', 'N': 'gene_terms'}, inplace=True)
-    # df_cnd_mrg.sort_values(by='pval', ascending=True, inplace=True)
-    # 
-    # # Display details for inidividual candidate genes
-    # all_overlap = set()
-    # cnd_gene_count = int(len(df_cnd))
-    # for gene in df_cnd_mrg['gene'].to_list():
-    #     df_gene = df_p2g.query('gene == @gene')
-    #     df_inter = df_gene.join(df_ancestors, how='inner', lsuffix='_gene', rsuffix='_proband').reset_index()
-    #     df_inter.drop_duplicates(subset='id', inplace=True, ignore_index=True)
-    #     all_overlap |= set(df_inter['term_gene'])
-    #     
-    #     x = len(df_inter.index.to_list())     # Intersection
-    #     M = len(set(df_p2g.index.to_list()))  # Total number of terms
-    #     n = len(df_ancestors.index.to_list()) # Proband ancestor terms
-    #     N = len(df_gene.index.to_list())      # Gene terms
-    #     rank = round((df_data[df_data['gene'] == gene]['pct_rank'].iloc[0] * 100), 1)
-    #     
-    #     print('Gene           = ' + gene)
-    #     print('Total terms    = ' + str(M))
-    #     print('Proband terms  = ' + str(n))
-    #     print('Gene terms     = ' + str(N))
-    #     print('Shared terms   = ' + str(x))
-    #     
-    #     pval = hypergeom.sf(x - 1, M, n, N)
-    #     pv_adj = pval * float(cnd_gene_count)
-    #     phred = -1 * math.log10(pv_adj)
-    #     print('pval           = ' + '%s' % float('%.2g' % pval))
-    #     print('pv_adj         = ' + '%s' % float('%.2g' % pv_adj))
-    #     print('')
-    #     print('-log10(pv_adj) = ' + str(round(phred, 1)))
-    #     print('Prcntl rank    = ' + str(rank))
-    # 
-    #     venn2([set(df_ancestors.index.to_list()),
-    #            set(df_gene.index.to_list())],
-    #           set_labels = ('Proband', gene))
-    #     plt.show()
-    # 
-    #     print(df_inter[['id', 'term_gene']].sort_values(by='id'))
-    # 
-    #     print('\n\n--------------------------------------------------------------------\n\n')
-    # 
-    # print("ALL OVERLAPS\n------------\n")
-    # all_overlap = list(all_overlap)
-    # all_overlap.sort()
-    # print('\n'.join(all_overlap))
-    # 
-    # # Show Hypergeometric Details for Top-10 Proband/Gene Overlaps
-    # print(df_data.sort_values(by='pval'))
-    # 
-    # # Display details for individual genes in top-10 proband/gene overlaps
-    # all_overlap = set()
-    # top_genes = df_data.sort_values(by='pval').head(10)
-    # top_genes = top_genes['gene'].to_list()
-    # 
-    # for gene in top_genes:
-    #     df_gene = df_p2g.query('gene == @gene')
-    #     df_inter = df_gene.join(df_ancestors, how='inner', lsuffix='_gene', rsuffix='_proband').reset_index()
-    #     df_inter.drop_duplicates(subset='id', inplace=True, ignore_index=True)
-    #     all_overlap |= set(df_inter['term_gene'])
-    #     
-    #     x = len(df_inter.index.to_list())     # Intersection
-    #     M = len(set(df_p2g.index.to_list()))  # Total number of terms
-    #     n = len(df_ancestors.index.to_list()) # Proband terms
-    #     N = len(df_gene.index.to_list())      # Gene terms
-    #     rank = round(df_data[df_data['gene'] == gene]['pct_rank'].iloc[0] * 100)
-    #     
-    #     print('Gene = ' + gene)
-    #     print('Total terms   = ' + str(M))
-    #     print('Proband terms = ' + str(n))
-    #     print('Gene terms    = ' + str(N))
-    #     print('Shared terms  = ' + str(x))
-    #     
-    #     pval = hypergeom.sf(x - 1, M, n, N)
-    #     pv_adj = pval * cnd_gene_count
-    #     phred = -1 * math.log10(pv_adj)
-    #     print('pval           = ' + '%s' % float('%.2g' % pval))
-    #     print('pv_adj         = ' + '%s' % float('%.2g' % pv_adj))
-    #     print('')
-    #     print('-log10(pv_adj) = ' + str(round(phred, 1)))
-    #     print('Prcntl rank    = ' + str(rank))
-    #     
-    #     venn2([set(df_ancestors.index.to_list()),
-    #            set(df_gene.index.to_list())],
-    #           set_labels = ('Proband', gene))
-    #     plt.show()
-    #     
-    #     print(df_inter[['id', 'term_gene']])
-    #     
-    #     print('\n\n--------------------------------------------------------------------\n\n')
-    # 
-    # print("ALL OVERLAPS\n------------\n")
-    # all_overlap = list(all_overlap)
-    # all_overlap.sort()
-    # print('\n'.join(all_overlap))
+    # Fix ordering here
+    #df_all_lcas = pd.DataFrame(all_lcas)
+    df_lcas = df_lcas.loc[:,['gene', 'ic', 'prb_term', 'gene_term', 'lca_term', 'anc_term', 'prb_id', 'gene_id', 'lca_id', 'anc_id', 'shpl', 'count', 'freq']]
+    print(df_lcas.to_csv(sep='\t', index=False))
 
 def get_lcas(prb_id, genes, pgG, pgUG):
 
     lcas = []
     for gene_id in genes:
         lca = nx.lowest_common_ancestor(pgG, gene_id, prb_id)
+        paths = nx.all_simple_paths(pgG, 'HP:0000118', prb_id)
+        anc = lca
+        for path in paths:
+            if (1 < len(path)):
+                anc = path[1]
+                break
         shpl = nx.shortest_path_length(pgUG, gene_id, prb_id)
-        lcas.append((prb_id, gene_id, lca, shpl))
+        lcas.append((anc, prb_id, gene_id, lca, shpl))
     return lcas
-    
+
 if __name__ == "__main__":
     main()
-
